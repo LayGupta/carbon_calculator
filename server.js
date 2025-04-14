d2e5e68bcf83da37614a5ebd3bef1165c7ac434e
Lay Gupta <136834019+LayGupta@users.noreply.github.com>
2025-04-14 14:09:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const connectDB = require('./backend/config/db');// idhar change kiya hai 
+const connectDB = require('./backend/config/db');// idhar change kiya hai ###
 
 // Connect to MongoDB
 connectDB();
@@ -15,7 +15,7 @@ app.use(express.json()); // Use express.json() instead of bodyParser.json()
 app.use(express.urlencoded({ extended: true })); // To handle URL-encoded data
 
 // Import and use routes
-const apiRoutes = require('./routes/api');
+const apiRoutes = require('./backend/routes/api');// aur idhar###
 app.use('/api', apiRoutes);
 
 // Set up the server
